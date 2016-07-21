51d721f3d91a3f40521dbe4998571f92dd490dd4
Achtung82 <l.hagmar@gmail.com>
2016-07-21 23:42:40 +0200

    Påbörjat adminläge för användare

---
+++ src/AKCore/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AKCore.DataModel;
+using AKCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKCore.Controllers
+{
+    [Route("User")]
+    [Authorize(Roles = "SuperNintendo")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AkUser> _userManager;
+
+        public UserController(
+            UserManager<AkUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Index(UsersModel model)
+        {
+            var users = _userManager.Users.ToList();
+            if (model.SearchPhrase != null)
+            {
+                users = users.Where(x => x.UserName.Contains(model.SearchPhrase)).ToList();
+            }
+            foreach (var user in users)
+            {
+                model.Roles[user.UserName] = await _userManager.GetRolesAsync(user);
+            }
+
+            model.Users = users;
+            return View(model);
+        }
+    }
+}
---
+++ src/AKCore/Models/UsersModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AKCore.DataModel;
+
+namespace AKCore.Models
+{
+    public class UsersModel
+    {
+        public List<AkUser> Users { get; set; }
+        public string SearchPhrase { get; set; }
+        public Hashtable Roles { get; set; }
+
+        public UsersModel()
+        {
+            Roles=new Hashtable();
+        }
+    }
+}
