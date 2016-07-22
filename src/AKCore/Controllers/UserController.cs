c56c696faf71d2128ecdeca35f90ba00d1b1d8e4
Achtung82 <l.hagmar@gmail.com>
2016-07-22 22:27:12 +0200

    Kan nu lägga till användare, dock ej ta bort än

--- src/AKCore/Controllers/UserController.cs
+++ src/AKCore/Controllers/UserController.cs
@@ -3,9 +3,7 @@ using System.Threading.Tasks;
 using AKCore.DataModel;
 using AKCore.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AKCore.Controllers
@@ -24,6 +22,7 @@ namespace AKCore.Controllers
 
         public async Task<ActionResult> Index(UsersModel model)
         {
+            ViewBag.Title = "Användare";
             var users = _userManager.Users.ToList();
             if (model.SearchPhrase != null)
             {
@@ -36,6 +35,38 @@ namespace AKCore.Controllers
 
             model.Users = users;
             return View(model);
+        }
+        [Route("UserList")]
+        public async Task<ActionResult> UserList(UsersModel model)
+        {
+            var users = _userManager.Users.ToList();
+            if (model.SearchPhrase != null)
+            {
+                users = users.Where(x => x.UserName.Contains(model.SearchPhrase)).ToList();
+            }
+            foreach (var user in users)
+            {
+                model.Roles[user.UserName] = await _userManager.GetRolesAsync(user);
+            }
+
+            model.Users = users;
+            return PartialView("_UserList", model);
+        }
+
+        [Route("CreateUser")]
+        public async Task<ActionResult> CreateUser(string userName, string password)
+        {
+            var newUser = new AkUser {UserName = userName};
+
+            var createRes = await _userManager.CreateAsync(newUser, password);
+            if (!createRes.Succeeded)
+            {
+                return Json(new {success = false, message = createRes.ToString()});
+            }
+
+            await _userManager.AddToRoleAsync(newUser, "Medlem");
+
+            return Json(new {success = true, message = "Användare skapades"});
         }
     }
 }
---
+++ src/AKCore/Scripts/users.js
@@ -0,0 +1,43 @@
+﻿$('#create-user')
+    .on('click',
+        function(e) {
+            e.preventDefault();
+            $('#createUserModal').modal('show');
+        });
+
+$('#create-user-form')
+    .on('submit',
+        function(e) {
+            e.preventDefault();
+            var form = $(this);
+            var success = $(".alert-success");
+            var error = form.find(".alert-danger");
+            $.ajax({
+                url: form.attr("action"),
+                type: form.attr("method"),
+                data: form.serialize(),
+                success: function (res) {
+                    if (res.success) {
+                        updateUserList();
+                        $('#createUserModal').modal('hide');
+                        success.text(res.message);
+                        success.slideDown().delay(3000).slideUp();
+                    } else {
+                        error.text(res.message);
+                        error.slideDown().delay(3500).slideUp();
+                    }
+                },
+                error: function (err) {
+                    error.text("Lyckades ej lägga till användare");
+                    error.slideDown().delay(3500).slideUp();
+                }
+            });
+
+        });
+
+function updateUserList(search) {
+    $.get("/User/UserList",
+     function (data) {
+         $("#user-table").empty().append(data);
+     });
+}
