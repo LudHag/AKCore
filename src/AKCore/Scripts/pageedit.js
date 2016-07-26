21218519ceb8180a2c88357e570fffcd18c2591f
Achtung82 <l.hagmar@gmail.com>
2016-07-26 18:15:27 +0200

    Initial widget-template

--- src/AKCore/Scripts/pageedit.js
+++ src/AKCore/Scripts/pageedit.js
@@ -82,3 +82,32 @@ $('.widget-choose')
             e.preventDefault();
             console.log('do something');
         });
+if($(".mce-content").length>0){
+    var options = {
+        selector: ".mce-content",
+        theme: "modern",
+        plugins: [
+            "advlist link image imagetools lists charmap print hr anchor spellchecker searchreplace wordcount code fullscreen media nonbreaking",
+            "table contextmenu directionality emoticons template textcolor paste textcolor colorpicker textpattern"
+        ],
+        toolbar1:
+            "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect code",
+        toolbar2:
+            "searchreplace bullist | undo redo | link unlink image | hr removeformat | charmap emoticons |  fullscreen ",
+        menubar: false,
+        elementpath: false,
+        convert_urls: false,
+        toolbar_items_size: "small",
+        width: "500",
+        height: "200",
+        content_css: "/css/akstyle.css",
+        body_class: "body-content",
+        file_browser_callback: function (field_name, url, type, win) {
+            if (type === "image") {
+                console.log('hej');
+            }
+        }
+    };
+    tinymce.init(options);
+
+}
