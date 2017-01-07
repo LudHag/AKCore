0fbafac4097f290aad6419e733aead65eb8d9a2f
Achtung82 <l.hagmar@gmail.com>
2017-01-07 01:20:16 +0100

    Fixar bugg

---
+++ src/AKCore/Migrations/20170107001902_otherinstforsignup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AKCore.Migrations
+{
+    public partial class otherinstforsignup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OtherInstruments",
+                table: "SignUps",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OtherInstruments",
+                table: "SignUps");
+        }
+    }
+}
