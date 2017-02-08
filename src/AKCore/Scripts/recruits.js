31d5d363aec3245d835e382c6bb30ad4248e6137
Achtung82 <l.hagmar@gmail.com>
2017-02-08 20:35:09 +0100

    Mer styling av rekryteringsinfo

--- src/AKCore/Scripts/recruits.js
+++ src/AKCore/Scripts/recruits.js
@@ -16,7 +16,7 @@ function RecruitList(data, dom) {
     recruitKeys.forEach(function (id) {
         self.recruits[id]=new Recruit(data[id]);
     });
-    this.dom.append($('<div class="row recruit-head"><div class="col-md-2">Skapad</div><div class="col-md-2">Namn</div><div class="col-md-1">Instrument</div><div class="col-md-3 contact">Kontaktinformation</div><div class="col-md-3 other">Övrigt</div><div class="col-md-1 actions"></div>'));
+    this.dom.append($('<div class="row recruit-head"><div class="col-md-2">Skapad</div><div class="col-md-2">Namn</div><div class="col-md-2">Instrument</div><div class="col-md-2 contact">Kontaktinformation</div><div class="col-md-3 other">Övrigt</div><div class="col-md-1 actions"></div>'));
     this.render();
 };
 
@@ -30,11 +30,11 @@ RecruitList.prototype.render = function () {
 
 function Recruit(data) {
     this.data = data;
-    this.dom = $('<div class="row recruit">');
+    this.dom = $('<div class="row recruit hover-grey">');
     this.dom.append($('<div class="col-md-2">' + data.created.getFullYear() + '-' + ('0' + (data.created.getMonth() + 1)).slice(-2) + '-' + ('0' + data.created.getDate()).slice(-2) + '</div>'));
     this.dom.append($('<div class="col-md-2">' + data.fname + ' ' + data.lname + '</div>'));
-    this.dom.append($('<div class="col-md-1">' + data.instrument + '</div>'));
-    this.dom.append($('<div class="col-md-3 contact">' + data.email + (data.email.length > 1 ? '<br>' : '') + data.phone + '</div>'));
+    this.dom.append($('<div class="col-md-2">' + data.instrument + '</div>'));
+    this.dom.append($('<div class="col-md-2 contact">' + data.email + (data.email.length > 1 ? '<br>' : '') + data.phone + '</div>'));
     this.dom.append($('<div class="col-md-3 other">' + data.other + '</div>'));
     this.dom.append($('<div class="col-md-1 actions"><input type="checkbox" /></div>'));
 };
