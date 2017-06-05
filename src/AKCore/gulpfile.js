dc9347635cc35a66ffbf2c7eaee3eeb0379cb2fc
Achtung82 <l.hagmar@gmail.com>
2017-06-06 00:52:15 +0200

    Kopplar om bootstrap till sass

--- src/AKCore/gulpfile.js
+++ src/AKCore/gulpfile.js
@@ -1,6 +1,7 @@
 /// <binding BeforeBuild='less, scripts' />
 var gulp = require('gulp');
 var less = require('gulp-less');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -8,14 +9,12 @@ var autoprefixer = require('gulp-autoprefixer');
 var path = require('path');
 var plumber = require('gulp-plumber');
 
-gulp.task('less', function () {
-    return gulp.src('./Styles/akstyle.less')
-    .pipe(plumber())
-    .pipe(less({
-        paths: [path.join(__dirname, 'less', 'includes')]
-    }))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./wwwroot/css/'));
+gulp.task('sass', function () {
+    return gulp.src('./Styles/akstyle.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./wwwroot/css/'));
 });
 gulp.task('scripts', function () {
     return gulp.src([
