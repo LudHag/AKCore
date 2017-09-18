494cca75806fcf6ff0f0b6d82fd53e0d549e0b94
Achtung82 <l.hagmar@gmail.com>
2017-09-18 23:42:48 +0200

    Så här långt

--- src/AKCore/Controllers/UpcomingController.cs
+++ src/AKCore/Controllers/UpcomingController.cs
@@ -39,8 +39,7 @@ namespace AKCore.Controllers
             }
             var model = new UpcomingModel
             {
-                Events = _db.Events.OrderBy(x => x.Day).ThenBy(x => x.Starts.TimeOfDay)
-                    .Include(x => x.SignUps)
+                Events = _db.Events.Include(x => x.SignUps).OrderBy(x => x.Day).ThenBy(x => x.Starts.TimeOfDay)
                     .Where(x => loggedIn || (x.Type == "Spelning"))
                     .Where(x => loggedIn || (!x.Secret))
                     .Where(x => x.Day >= DateTime.UtcNow.Date)
