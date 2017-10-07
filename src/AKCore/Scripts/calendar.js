09dee71a3529cd6497bac663043439df81d8149d
Achtung82 <l.hagmar@gmail.com>
2017-10-07 22:35:44 +0200

    Renderar kalender någerlunda utan styling eller innehåll

--- src/AKCore/Scripts/calendar.js
+++ src/AKCore/Scripts/calendar.js
@@ -20,7 +20,7 @@ function Calendar(container) {
 
 
 Calendar.prototype.render = function () {
-    this.shownMonth.render();
+    this.container.append(this.shownMonth.render());
 };
 
 function Month(year, month) {
@@ -33,42 +33,55 @@ function Month(year, month) {
     firstDayOfCalendar.setDate(firstDayOfMonth.getDate() - firstDayWeekDay);
     var monday = new Date(firstDayOfCalendar.getTime());
     while (monday < lastDayOfMonth) {
-        var week = new Week(monday);
+        var week = new Week(monday, firstDayOfMonth, lastDayOfMonth);
         this.weeks.push(week);
         monday = new Date(monday.getTime() + timeWeek);
     }
+    this.dom = $('<table class="month table table-bordered"></div>');
 };
 
 Month.prototype.render = function () {
+    var self = this;
+    var tableHeading =
+        $('<thead> <tr><th>Måndag</th> <th>Tisdag</th> <th>Onsdag</th> <th>Torsdag</th> <th>Fredag</th> <th>Lördag</th> <th>Söndag</th> </tr> </thead>');
+    self.dom.append(tableHeading);
+    var tableBody = $('<tbody></tbody>');
     this.weeks.forEach(function (week) {
-        console.log(week.render());
+        tableBody.append(week.render());
     });
+    self.dom.append(tableBody);
+    return this.dom;
 };
 
 
 
-function Week(firstDay) {
+function Week(firstDay, firstDayOfMonth, lastDayOfMonth) {
     this.firstDay = firstDay;
     this.days = [];
     for (var i = 0; i < 7; i++) {
-        var date = new Date(this.firstDay.getTime() + timeDay*i);
-        var day = new Day(date);
+        var date = new Date(this.firstDay.getTime() + timeDay * i);
+        var inMonth = date >= firstDayOfMonth && date <= lastDayOfMonth;
+        var day = new Day(date, inMonth);
         this.days.push(day);
     }
+    this.dom = $('<tr class="week"></div>');
 };
 
 Week.prototype.render = function () {
-    var result = '';
+    var self = this;
     this.days.forEach(function (day) {
-        result += day.render() + '---';
+        self.dom.append(day.render());
     });
-    return result.substring(0, result.length - 3);
+    return this.dom;
 };
 
-function Day(day) {
+function Day(day, inMonth) {
     this.day = day;
+    this.inMonth = inMonth;
+    this.dom = $('<td class="day">' + this.day.getDate() + '</div>');
+    if (!inMonth) this.dom.addClass('outside');
 };
 
 Day.prototype.render = function () {
-    return this.day.getDate();
+    return this.dom;
 };
