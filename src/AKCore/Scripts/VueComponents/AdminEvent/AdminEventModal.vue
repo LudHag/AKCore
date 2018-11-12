cd689836bbf0dec6f54b65c540d58e46ec7d17c6
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2018-11-12 21:43:54 +0100

    Making modal component

--- src/AKCore/Scripts/VueComponents/AdminEvent/AdminEventModal.vue
+++ src/AKCore/Scripts/VueComponents/AdminEvent/AdminEventModal.vue
@@ -1,113 +1,107 @@
 ﻿<template>
-    <div class="modal show" @click="modalClose">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" @click.prevent="close">&times;</button>
-                    <h4 class="modal-title">Redigera händelse</h4>
+   <modal :show-modal="showModal" header="'Redigera händelse'" @close="close">
+        <form slot="body" action="/AdminEvent/Edit" method="post" @submit.prevent="formSubmit">
+            <div class="modal-body">
+                <div class="alert alert-danger" style="display: none;">
                 </div>
-                <form action="/AdminEvent/Edit" method="post" @submit.prevent="formSubmit">
-                    <div class="modal-body">
-                        <div class="alert alert-danger" style="display: none;">
-                        </div>
-                        <input type="hidden" name="Id" :value="eventId" />
-                        <div class="form-group">
-                            <label>Typ</label>
-                            <select class="form-control" name="Type" 
-                                    v-model="eventType" required>
-                                <option value="">Typ av händelse</option>
-                                <option v-for="e in eventTypes">{{e}}</option>
-                            </select>
-                        </div>
-                        <div class="editeventbody" v-if="eventType">
-                            <div class="form-group" v-if="spelningFest">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <label>Namn</label>
-                                        <input class="form-control" v-model="event.name" name="Name">
-                                    </div>
-                                    <div class="col-sm-6" v-if="eventType === 'Spelning'">
-                                        <label></label>
-                                        <div class="checkbox checkbox-center">
-                                            <label>
-                                                <input type="checkbox" v-model="event.secret" name="Secret"> Hemlig spelning
-                                            </label>
-                                        </div>
-                                    </div>
+                <input type="hidden" name="Id" :value="eventId" />
+                <div class="form-group">
+                    <label>Typ</label>
+                    <select class="form-control" name="Type" 
+                            v-model="eventType" required>
+                        <option value="">Typ av händelse</option>
+                        <option v-for="e in eventTypes" :key="e">{{e}}</option>
+                    </select>
+                </div>
+                <div class="editeventbody" v-if="eventType">
+                    <div class="form-group" v-if="spelningFest">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label>Namn</label>
+                                <input class="form-control" v-model="event.name" name="Name">
+                            </div>
+                            <div class="col-sm-6" v-if="eventType === 'Spelning'">
+                                <label></label>
+                                <div class="checkbox checkbox-center">
+                                    <label>
+                                        <input type="checkbox" v-model="event.secret" name="Secret"> Hemlig spelning
+                                    </label>
                                 </div>
                             </div>
-                            <div class="form-group" v-if="spelningFest">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <label>Plats</label>
-                                        <input class="form-control" v-model="event.place" name="Place">
-                                    </div>
-                                    <div class="col-sm-6" v-if="eventType === 'Spelning'">
-                                        <label>Stå- eller gåspelning</label>
-                                        <select class="form-control" v-model="event.stand" name="Stand" required>
-                                            <option value="">Välj speltyp</option>
-                                            <option v-for="type in spelTyper">{{type}}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                        <label>Dag</label>
-                                        <datepicker input-class="form-control" v-model="event.dayDate" name="Day" required></datepicker>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <label>Vid hålan</label>
-                                        <input class="form-control" type="time" v-model="event.halanTime" name="Halan" value="00:00">
-                                    </div>
-                                    <div class="col-sm-3" v-if="spelningKarhus">
-                                        <label>På plats</label>
-                                        <input class="form-control" type="time" v-model="event.thereTime" name="There" value="00:00">
-                                    </div>
-                                    <div class="col-sm-3" v-if="eventType === 'Spelning'">
-                                        <label>Spelning</label>
-                                        <input class="form-control" type="time" name="Starts" v-model="event.startsTime" value="00:00" required>
-                                    </div>
-                                    <div class="col-sm-6" v-if="repFika && eventType !== 'Fikarep'">
-                                        <label>Fika</label>
-                                        <select class="form-control" v-model="event.fika" name="Fika">
-                                            <option value="">Välj en sektion</option>
-                                            <option v-for="s in sektioner">{{s}}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group" v-if="eventType === 'Spelning'">
-                                <label>Beskrivning</label>
-                                <textarea class="form-control" v-model="event.description" name="Description"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Intern beskrivning</label>
-                                <textarea class="form-control" v-model="event.internalDescription" name="InternalDescription"></textarea>
-                            </div>
                         </div>
-
                     </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-default" @click.prevent="close">Stäng</button>
-                        <button v-if="!old || !eventId" type="submit" class="btn btn-primary">Spara</button>
+                    <div class="form-group" v-if="spelningFest">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label>Plats</label>
+                                <input class="form-control" v-model="event.place" name="Place">
+                            </div>
+                            <div class="col-sm-6" v-if="eventType === 'Spelning'">
+                                <label>Stå- eller gåspelning</label>
+                                <select class="form-control" v-model="event.stand" name="Stand" required>
+                                    <option value="">Välj speltyp</option>
+                                    <option v-for="type in spelTyper" :key="type">{{type}}</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-                </form>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <label>Dag</label>
+                                <datepicker input-class="form-control" v-model="event.dayDate" name="Day" required></datepicker>
+                            </div>
+                            <div class="col-sm-3">
+                                <label>Vid hålan</label>
+                                <input class="form-control" type="time" v-model="event.halanTime" name="Halan" value="00:00">
+                            </div>
+                            <div class="col-sm-3" v-if="spelningKarhus">
+                                <label>På plats</label>
+                                <input class="form-control" type="time" v-model="event.thereTime" name="There" value="00:00">
+                            </div>
+                            <div class="col-sm-3" v-if="eventType === 'Spelning'">
+                                <label>Spelning</label>
+                                <input class="form-control" type="time" name="Starts" v-model="event.startsTime" value="00:00" required>
+                            </div>
+                            <div class="col-sm-6" v-if="repFika && eventType !== 'Fikarep'">
+                                <label>Fika</label>
+                                <select class="form-control" v-model="event.fika" name="Fika">
+                                    <option value="">Välj en sektion</option>
+                                    <option v-for="s in sektioner">{{s}}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group" v-if="eventType === 'Spelning'">
+                        <label>Beskrivning</label>
+                        <textarea class="form-control" v-model="event.description" name="Description"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Intern beskrivning</label>
+                        <textarea class="form-control" v-model="event.internalDescription" name="InternalDescription"></textarea>
+                    </div>
+                </div>
+
             </div>
-        </div>
-    </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" @click.prevent="close">Stäng</button>
+                <button v-if="!old || !eventId" type="submit" class="btn btn-primary">Spara</button>
+            </div>
+        </form>
+    </modal>
 </template>
 <script>
     import Constants from '../../constants';
     import Datepicker from 'vuejs-datepicker';
     import ApiService from '../../services/apiservice';
+    import Modal from '../Modal';
 
     export default {
-        props: ['selectedEvent', 'old'],
+        props: ['selectedEvent', 'old', 'showModal'],
         components: {
-            Datepicker
+            Datepicker,
+            Modal
         },
         data() {
             return {
@@ -116,11 +110,6 @@
             }
         },
         methods: {
-            modalClose() {
-                if (event.target.classList.contains("modal")) {
-                    this.$emit("close");
-                }
-            },
             close() {
                 this.$emit("close");
             },
@@ -136,6 +125,32 @@
                 ApiService.postByObject("/AdminEvent/Edit", this.event, error, success, () => {
                     self.$emit("update");
                 });
+            }
+        },
+        watch:{
+            selectedEvent() {
+                if(this.showModal){
+                    const today = new Date();
+                    this.eventType = this.selectedEvent ? this.selectedEvent.type : "";
+                    this.event = this.selectedEvent ? Object.assign({}, this.selectedEvent) :
+                        {
+                            type: "",
+                            name: "",
+                            place: "",
+                            description: "",
+                            internalDescription: "",
+                            year: today.getFullYear(),
+                            month: today.getMonth() + 1,
+                            dayDate: today,
+                            fika: "",
+                            halanTime: "00:00",
+                            thereTime: "00:00",
+                            startsTime: "00:00",
+                            stand: "",
+                            secret: false
+                        };
+                    this.event.dayDate = new Date(this.event.dayDate);
+                }
             }
         },
         computed: {
@@ -165,28 +180,6 @@
                     this.eventType === "Kårhusrep" ||
                     this.eventType === "Fikarep";
             }
-        },
-        created() {
-            const today = new Date();
-            this.eventType = this.selectedEvent ? this.selectedEvent.type : "";
-            this.event = this.selectedEvent ? Object.assign({}, this.selectedEvent) :
-                {
-                    type: "",
-                    name: "",
-                    place: "",
-                    description: "",
-                    internalDescription: "",
-                    year: today.getFullYear(),
-                    month: today.getMonth() + 1,
-                    dayDate: today,
-                    fika: "",
-                    halanTime: "00:00",
-                    thereTime: "00:00",
-                    startsTime: "00:00",
-                    stand: "",
-                    secret: false
-                };
-            this.event.dayDate = new Date(this.event.dayDate);
         }
     }
 </script>
---
+++ src/AKCore/Scripts/VueComponents/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <transition name="modal">
+      <div v-if="showModal" class="modal-wrapper">
+        <div class="modal show" @click="modalClose">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" @click.prevent="close">&times;</button>
+                            <h4 class="modal-title">Redigera händelse</h4>
+                        </div>
+                        <slot name="body"></slot>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-backdrop fade in"></div>
+        </div>
+    </transition>
+</template>
+<script>
+
+  export default {
+        props: ['showModal', 'header'],
+        methods: {
+            modalClose() {
+                if (event.target.classList.contains("modal")) {
+                    this.$emit("close");
+                }
+            },
+            close(){
+                this.$emit("close");
+            }
+        }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+.modal-enter, .modal-leave-active {
+    opacity: 0;
+}
+
+.modal-enter .modal-dialog,
+.modal-leave-active .modal-dialog {
+    transform: translateY(-30%);
+}
+.modal-wrapper {
+    transition: all .3s ease;
+}
+</style>
