faec153726fa0075cb8b85ccb8a6e25603b3a64c
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2018-11-16 22:26:20 +0100

    Creating app

---
+++ src/AKCore/Scripts/VueComponents/AlbumEdit/AlbumEditApp.vue
@@ -0,0 +1,26 @@
+﻿<template>
+    <div id="album-edit-app">
+        Woop woop
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                albums: null
+            }
+        },
+        created() {
+            //$.ajax({
+            //    url: "/Profile/ProfileData",
+            //    type: "GET",
+            //    success: function (res) {
+            //        self.profileData = res;
+            //    }
+            //});
+        }
+    }
+</script>
+<style lang="scss">
+
+</style>
