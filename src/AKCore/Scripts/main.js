ee51971cf6229bdec7c50a8a82cb699118404481
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2018-12-16 16:25:10 +0100

    Removing old menu edit code

--- src/AKCore/Scripts/main.js
+++ src/AKCore/Scripts/main.js
@@ -2,6 +2,5 @@ import "../Styles/akstyle.scss";
 import "./general.js";
 import "./vueapps.js";
 import "./musicplayer.js";
-//import "./Admin/menuedit.js";
 import "./Admin/media.js";
 import "./Admin/recruits.js";
