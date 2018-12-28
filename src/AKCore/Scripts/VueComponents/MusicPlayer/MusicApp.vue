15ae993273e7ddb83e25fd1c9604f1bf997c641a
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2018-12-28 20:41:47 +0100

    Creating code for initial music player app

---
+++ src/AKCore/Scripts/VueComponents/MusicPlayer/MusicApp.vue
@@ -0,0 +1,17 @@
+﻿<template>
+  <div id="music-app">
+    woop woop
+  </div>
+</template>
+<script>
+import ApiService from "../../services/apiservice";
+
+export default {
+  created() {
+    console.log(widgetAlbums);
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/AKCore/Scripts/main.js
+++ src/AKCore/Scripts/main.js
@@ -2,5 +2,5 @@ import "../Styles/akstyle.scss";
 import "./general.js";
 import "./Vendor/object-assign-poly";
 import "./vueapps.js";
-import "./musicplayer.js";
+// import "./musicplayer.js";
 import "./Admin/recruits.js";
