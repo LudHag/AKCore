c0c7c625c0ce6ef678bd8c8628f78a96da2200e8
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2018-12-28 20:23:26 +0100

    extending album props

--- src/AKCore/DataModel/AKContext.cs
+++ src/AKCore/DataModel/AKContext.cs
@@ -125,6 +125,8 @@ namespace AKCore.DataModel
         public int Id { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
+        public string Year { get; set; }
+        public string Category { get; set; }
         public DateTime Created { get; set; }
         public DateTime Released { get; set; }
         public List<Track> Tracks { get; set; }
---
+++ src/AKCore/Migrations/20181228192243_albumprops.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AKCore.Migrations
+{
+    public partial class albumprops : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Albums",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Albums",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Albums");
+        }
+    }
+}
