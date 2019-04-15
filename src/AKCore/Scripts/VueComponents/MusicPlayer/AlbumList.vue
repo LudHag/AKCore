5bd82eadde7b58d74defc19a53ffadc33d228459
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2019-04-15 21:22:52 +0200

    Track search

--- src/AKCore/Scripts/VueComponents/MusicPlayer/AlbumList.vue
+++ src/AKCore/Scripts/VueComponents/MusicPlayer/AlbumList.vue
@@ -1,14 +1,40 @@
 ﻿<template>
   <div class="album-list">
-    <div class="category" v-for="category in categoryList" :key="category[0].category">
-      <h2 v-if="categoryList.length > 1">{{category[0].category}}</h2>
-      <div class="category-list" :class="{'no-cats': categoryList.length === 1}">
-        <div class="album-element" v-for="album in category" :key="album.id">
-          <a class="album-link" href="#" @click.prevent="$emit('select', album)">
-            <img class="album-img" :src="album.image">
-            <p class="album-name">{{album.name}}</p>
-          </a>
+    <div class="form-inline track-search">
+      <div class="form-group">
+        Sök låtar:
+        <input
+          type="text"
+          v-model="searchQuery"
+          class="form-control"
+          placeholder="Sök här"
+        >
+      </div>
+    </div>
+    <div class="categories" v-if="!filteredAlbums">
+      <div class="category" v-for="category in categoryList" :key="category[0].category">
+        <h2 v-if="categoryList.length > 1">{{category[0].category}}</h2>
+        <div class="category-list" :class="{'no-cats': categoryList.length === 1}">
+          <div class="album-element" v-for="album in category" :key="album.id">
+            <a class="album-link" href="#" @click.prevent="$emit('select', album)">
+              <img class="album-img" :src="album.image">
+              <p class="album-name">{{album.name}}</p>
+            </a>
+          </div>
         </div>
+      </div>
+    </div>
+    <div class="tracks" v-if="filteredAlbums">
+      <div v-for="album in filteredAlbums" :key="album.id">
+        <h2>{{album.name}}</h2>
+        <a
+          href="#"
+          class="track"
+          @click.prevent="clickTrack"
+          v-for="track in album.tracks"
+          :key="track.id"
+          v-html="track.name"
+        ></a>
       </div>
     </div>
   </div>
@@ -18,30 +44,81 @@ import { groupBy, nameCompare } from "../../utils/functions";
 
 export default {
   props: ["albums"],
-   data() {
+  data() {
     return {
-      categoriesEnabled: true
+      categoriesEnabled: true,
+      searchQuery: ""
     };
   },
   computed: {
     categoryList() {
       const keys = Object.keys(this.albums);
-      const albums = keys.map(key => {
-        return Object.assign({ id: key, category: "Övrigt" }, this.albums[key]);
-      }).sort(nameCompare);
-      window.hej = albums;
-      if(!this.categoriesEnabled || keys.length < 6) {
+      const albums = keys
+        .map(key => {
+          return Object.assign(
+            { id: key, category: "Övrigt" },
+            this.albums[key]
+          );
+        })
+        .sort(nameCompare);
+      if (!this.categoriesEnabled || keys.length < 6) {
         return [albums];
       }
       const categories = groupBy(albums, "category");
       return Object.keys(categories)
         .sort()
         .map(cat => categories[cat]);
+    },
+    filteredAlbums() {
+      if (!this.searchQuery) {
+        return null;
+      }
+      const lowerQuery = this.searchQuery.toLowerCase();
+      let tracks = [];
+      const keys = Object.keys(this.albums);
+      const albums = keys
+        .map(key => {
+          const trackKeys = Object.keys(this.albums[key].tracks);
+          const filteredTracks = trackKeys
+            .map(tKey => this.albums[key].tracks[tKey])
+            .filter(track => track.name.toLowerCase().indexOf(lowerQuery) > -1);
+          return Object.assign({}, this.albums[key], { tracks: filteredTracks });
+        })
+        .filter(album => album.tracks.length > 0);
+      return albums;
+    }
+  },
+  methods: {
+    clickTrack() {
+      console.log("click");
     }
   }
 };
 </script>
 <style lang="scss" scoped>
+.album-list {
+  margin-top: 10px;
+  position: relative;
+}
+.categories {
+  margin-top: 10px;
+}
+.track-search {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.tracks {
+  column-count: 2;
+  padding-top: 40px;
+  h2 {
+    color: #999;
+        font-size: 17px;
+  }
+}
+.track {
+  display: block;
+}
 .category-list {
   list-style-type: none;
   padding-left: 0;
@@ -50,7 +127,7 @@ export default {
   margin-top: 5px;
   justify-content: left;
   &.no-cats {
-     justify-content: center;
+    justify-content: center;
   }
 }
 
@@ -69,12 +146,12 @@ export default {
   }
 }
 h2 {
-      font-size: 20px;
-    line-height: 30px;
+  font-size: 20px;
+  line-height: 30px;
 }
 .album-name {
   text-align: center;
   font-size: 14px;
-      margin-top: 5px;
+  margin-top: 5px;
 }
 </style>
