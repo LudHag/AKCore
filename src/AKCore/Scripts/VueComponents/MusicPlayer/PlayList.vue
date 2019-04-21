3da893bc436939415e4a626b52e0253d6dc2e89c
Ludvi Hagmar <ludvig.hagmar@bombayworks.se>
2019-04-21 13:28:15 +0200

    Extractinbg player from playlist

--- src/AKCore/Scripts/VueComponents/MusicPlayer/PlayList.vue
+++ src/AKCore/Scripts/VueComponents/MusicPlayer/PlayList.vue
@@ -1,28 +1,12 @@
 ﻿
 <template>
   <div class="player-module">
-    <div class="playingnow" v-if="trackPlaying" v-html="nowPlayingText">Spelar nu: 03 Dansa i neon</div>
-    <div class="controls" v-if="trackPlaying">
-      <a
-        href="#"
-        @click.prevent="$emit('playpause')"
-        v-if="playing"
-        class="pauseplay glyphicon glyphicon-pause"
-      ></a>
-      <a
-        href="#"
-        @click.prevent="$emit('playpause')"
-        v-else
-        class="pauseplay glyphicon glyphicon-play"
-      ></a>
-      <div class="progress-container" ref="progress" @click.prevent="clickProgress">
-        <div class="music-progress">
-          <span class="music-progress-bar" :style="{ width: progress + '%' }"></span>
-        </div>
-      </div>
-      <div class="progresstime">{{timeDisplay}}</div>
-    </div>
-    <audio class="player" v-if="trackPlaying" ref="player" :src="trackPlaying.filepath"></audio>
+    <Player
+      :playing="playing"
+      :track-playing="trackPlaying"
+      @playpause="$emit('playpause')"
+      @next="next"
+    ></Player>
     <div class="playlist">
       <a
         :href="track.filepath"
@@ -39,20 +23,19 @@
   </div>
 </template>
 <script>
-import { fmtMSS, nameCompare } from "../../utils/functions";
+import { nameCompare } from "../../utils/functions";
+import Player from "./Player";
 export default {
   props: ["playList", "playing", "album"],
+  components: {
+    Player
+  },
   data() {
     return {
-      trackLength: 0,
-      timePlayed: 0,
       trackPlaying: null
     };
   },
   computed: {
-    timeDisplay() {
-      return fmtMSS(this.timePlayed) + "/" + fmtMSS(this.trackLength);
-    },
     progress() {
       if (this.timePlayed === 0 || this.trackLength === 0) {
         return 0;
@@ -65,36 +48,9 @@ export default {
       }
       const trackKeys = Object.keys(this.album.tracks);
       return trackKeys.map(key => this.album.tracks[key]).sort(nameCompare);
-    },
-    nowPlayingText() {
-      return "Spelar nu: " + this.trackPlaying.name;
-    }
-  },
-  watch: {
-    playing(val) {
-      const player = this.$refs.player;
-      if (val) {
-        player.play();
-        player.addEventListener("timeupdate", this.timeUpdate);
-        player.addEventListener("ended", this.endedListener);
-      } else {
-        player.pause();
-        player.removeEventListener("timeupdate", this.timeUpdate);
-        player.removeEventListener("ended", this.endedListener);
-      }
     }
   },
   methods: {
-    timeUpdate(event) {
-      const player = this.$refs.player;
-      this.trackLength = player.duration;
-      this.timePlayed = event.target.currentTime;
-    },
-    endedListener() {
-      this.trackLength = 0;
-      this.timePlayed = 0;
-      this.next();
-    },
     next() {
       const currentIndex = this.tracks.findIndex(
         track => track.id === this.trackPlaying.id
@@ -104,32 +60,13 @@ export default {
         this.trackPlaying = null;
       } else {
         this.trackPlaying = this.tracks[currentIndex + 1];
-        this.reloadTrack();
       }
-    },
-    clickProgress(event) {
-      const progressContainer = this.$refs.progress;
-      const player = this.$refs.player;
-      const decimalProgress = event.offsetX / progressContainer.offsetWidth;
-      const time = decimalProgress * player.duration;
-      this.trackLength = player.duration;
-      this.timePlayed = time;
-      player.currentTime = time;
     },
     selectTrack(track) {
       this.trackPlaying = track;
       if (!this.playing) {
         this.$nextTick(() => this.$emit("playpause"));
-      } else {
-        this.reloadTrack();
       }
-    },
-    reloadTrack() {
-      const player = this.$refs.player;
-      this.$nextTick(() => {
-        player.load();
-        player.play();
-      });
     }
   }
 };
@@ -140,50 +77,7 @@ export default {
 .player-module {
   flex-grow: 1;
   padding-left: 30px;
-
-  .controls {
-    border: 1px solid $akred;
-    border-radius: 7px;
-    padding: 7px;
-    margin-top: 3px;
-    margin-bottom: 12px;
-    display: flex;
-    align-items: center;
-  }
 }
-
-.pauseplay {
-  margin-right: 7px;
-}
-
-.progress-container {
-  flex-grow: 1;
-  height: 15px;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-}
-
-.progresstime {
-  min-width: 50px;
-  font-size: 12px;
-  text-align: right;
-  margin-left: 10px;
-}
-
-.music-progress {
-  height: 3px;
-  background-color: #c1bfbd;
-  width: 100%;
-
-  .music-progress-bar {
-    background-color: #6f6f6f;
-    display: block;
-    height: 100%;
-    transition: width ease 0.1s;
-  }
-}
-
 .playlist {
   height: 210px;
   overflow: auto;
@@ -207,40 +101,37 @@ export default {
     margin-right: 30px;
   }
 }
+.player-module {
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
 
-::-webkit-scrollbar {
-  width: 12px;
-}
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+  }
 
-::-webkit-scrollbar-track {
-  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  border-radius: 10px;
-}
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px #a5a2a0;
+  }
 
-::-webkit-scrollbar-thumb {
-  border-radius: 10px;
-  -webkit-box-shadow: inset 0 0 6px #a5a2a0;
-}
+  ::-moz-scrollbar {
+    width: 12px;
+  }
 
-::-moz-scrollbar {
-  width: 12px;
-}
+  ::-moz-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+  }
 
-::-moz-scrollbar-track {
-  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  border-radius: 10px;
-}
-
-::-webkit-scrollbar-thumb {
-  border-radius: 10px;
-  -webkit-box-shadow: inset 0 0 6px #a5a2a0;
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px #a5a2a0;
+  }
 }
 
 @media screen and (max-width: $screen-xs-max) {
-  .player {
-    width: 100%;
-  }
-
   .player-module {
     padding-top: 30px;
     padding-left: 0;
