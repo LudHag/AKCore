88dc175630b938b9c5f98670ebd327045dc17f55
Ludvig Hagmar <ludvig.hagmar@bombayworks.se>
2020-12-28 16:21:50 +0100

    refactor metadata

--- src/AKCore/Scripts/VueComponents/PageEdit/PageEdit.vue
+++ src/AKCore/Scripts/VueComponents/PageEdit/PageEdit.vue
@@ -2,69 +2,21 @@
   <div id="pageedit-app" v-if="pageModel">
     <div class="row">
       <form method="post" @submit.prevent="save">
-        <div class="col-md-6">
-          <div class="alert alert-danger" style="display: none;"></div>
-          <div class="alert alert-success" style="display: none;"></div>
-          <div class="form-group">
-            <label for="name">Namn</label>
-            <input
-              type="text"
-              class="form-control"
-              required="required"
-              name="Name"
-              placeholder="Namn"
-              v-model="pageModel.name"
-            />
-          </div>
-          <div class="form-group">
-            <label for="slug">Sidlänk</label>
-            <input
-              type="text"
-              class="form-control"
-              required="required"
-              name="Slug"
-              placeholder="Sidlänk"
-              v-model="pageModel.slug"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="slug">Inställningar</label>
-            <div class="checkbox">
-              <label> <input type="checkbox" /> Startsida </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" v-model="pageModel.loggedIn" /> Kräver
-                inloggning
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" v-model="pageModel.loggedOut" /> Enbart
-                utloggade
-              </label>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" v-model="pageModel.balettOnly" /> För
-                baletten
-              </label>
-            </div>
-          </div>
-          <button type="submit" class="btn btn-default">
-            Spara
-          </button>
-        </div>
+        <div class="alert alert-danger" style="display: none;"></div>
+        <div class="alert alert-success" style="display: none;"></div>
+        <page-meta v-model="pageModel"></page-meta>
       </form>
     </div>
   </div>
 </template>
 <script>
 import Constants from "../../constants";
+import PageMeta from "./PageMeta";
 
 export default {
+  components: {
+    PageMeta
+  },
   data() {
     return {
       pageModel: null
@@ -82,7 +34,7 @@ export default {
   },
   methods: {
     save(e) {
-      console.log(e);
+      console.log(this.pageModel.name);
     }
   }
 };
---
+++ src/AKCore/Scripts/VueComponents/PageEdit/PageMeta.vue
@@ -0,0 +1,60 @@
+﻿<template>
+  <div>
+    <div class="col-md-6">
+      <div class="form-group">
+        <label for="name">Namn</label>
+        <input
+          type="text"
+          class="form-control"
+          required="required"
+          name="Name"
+          placeholder="Namn"
+          v-model="value.name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="slug">Sidlänk</label>
+        <input
+          type="text"
+          class="form-control"
+          required="required"
+          name="Slug"
+          placeholder="Sidlänk"
+          v-model="value.slug"
+        />
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="form-group">
+        <label for="slug">Inställningar</label>
+        <div class="checkbox">
+          <label> <input type="checkbox" /> Startsida </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" v-model="value.loggedIn" /> Kräver inloggning
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" v-model="value.loggedOut" /> Enbart utloggade
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" v-model="value.balettOnly" /> För baletten
+          </label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-default">
+        Spara
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["value"]
+};
+</script>
+<style lang="scss"></style>
