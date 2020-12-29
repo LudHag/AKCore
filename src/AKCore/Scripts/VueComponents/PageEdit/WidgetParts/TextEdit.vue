3c9a4f501866eaa857e26ce6553d7f22f98f772d
Ludvig Hagmar <ludvig.hagmar@bombayworks.se>
2020-12-29 22:57:53 +0100

    extracxting text edit

---
+++ src/AKCore/Scripts/VueComponents/PageEdit/WidgetParts/TextEdit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="col-sm-6">
+      <textarea class="mce-content" v-html="value" @input="update"></textarea>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    update(event) {
+      this.$emit("input", event.target.value);
+    }
+  }
+};
+</script>
+<style lang="scss"></style>
--- src/AKCore/Scripts/VueComponents/PageEdit/Widgets/TextImage.vue
+++ src/AKCore/Scripts/VueComponents/PageEdit/Widgets/TextImage.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <div class="col-sm-6">
-      <textarea class="mce-content" v-html="value.text"></textarea>
-    </div>
+    <text-edit v-model="value.text"></text-edit>
     <image-edit v-model="value.image"></image-edit>
   </div>
 </template>
 <script>
 import ImageEdit from "../WidgetParts/ImageEdit.vue";
+import TextEdit from "../WidgetParts/TextEdit.vue";
 export default {
-  components: { ImageEdit },
+  components: { ImageEdit, TextEdit },
   props: ["value"]
 };
 </script>
