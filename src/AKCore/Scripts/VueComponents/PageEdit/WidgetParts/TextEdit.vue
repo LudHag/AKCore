c162cc68b18b621f7b392eb30ea9b92c19a2d912
Ludvig Hagmar <ludvig.hagmar@bombayworks.se>
2020-12-30 18:13:00 +0100

    Testing pell

--- src/AKCore/Scripts/VueComponents/PageEdit/WidgetParts/TextEdit.vue
+++ src/AKCore/Scripts/VueComponents/PageEdit/WidgetParts/TextEdit.vue
@@ -1,46 +1,78 @@
 <template>
   <div class="col-sm-6">
     <!-- <textarea class="mce-content" v-html="value" @input="update"></textarea> -->
-    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-      <button
-        class="fa fa-bold"
-        :class="{ 'is-active': isActive.bold() }"
-        @click="commands.bold"
-      ></button>
-    </editor-menu-bar>
-    <editor-content class="tipeditor" :editor="editor" @update="update" />
+    <div ref="pell" class="pell" />
   </div>
 </template>
 <script>
-import { Editor, EditorContent, EditorMenuBar } from "tiptap";
-import { Blockquote, Bold, Link } from "tiptap-extensions";
+import { exec, init } from "pell";
 
 export default {
-  components: { EditorContent, EditorMenuBar },
   props: ["value"],
   data() {
     return {
-      editor: new Editor({
-        extensions: [new Blockquote(), new Bold(), new Link()],
-        content: this.value,
-        onUpdate: this.update
-      })
+      editor: null
     };
   },
-  methods: {
-    update(state) {
-      this.$emit("input", state.getHTML());
-    }
-  },
-  beforeDestroy() {
-    this.editor.destroy();
+  mounted() {
+    this.editor = init({
+      element: this.$refs.pell,
+      onChange: html => {
+        this.$emit("input", html);
+      },
+      defaultParagraphSeparator: "p",
+      classes: {
+        content: "pell-content body-content",
+        button: "pell-button",
+        selected: "pell-button-selected"
+      },
+      actions: [
+        {
+          name: "backColor",
+          icon: '<div style="background-color:pink;">A</div>',
+          title: "Highlight Color",
+          result: () => exec("backColor", "pink")
+        },
+        "bold",
+        "italic",
+        "underline",
+        "strikethrough",
+        "heading1",
+        "heading2",
+        "paragraph",
+        "quote",
+        "olist",
+        "ulist",
+        "code",
+        "line",
+        {
+          name: "image",
+          result: () => {
+            const url = window.prompt("Enter the image URL");
+            if (url) pell.exec("insertImage", url);
+          }
+        },
+        {
+          name: "link",
+          result: () => {
+            const url = window.prompt("Enter the link URL");
+          }
+        }
+      ]
+    });
+    this.editor.content.innerHTML = this.value;
   }
 };
 </script>
 <style lang="scss" scoped>
-.tipeditor /deep/ .ProseMirror {
-  min-height: 230px;
-  padding: 5px;
-  border: 1px solid green;
+@import "../../../../node_modules/pell/src/pell";
+.pell {
+  border: 2px solid #000;
+  border-radius: 0;
+  box-shadow: none;
+}
+/deep/ .pell-content {
+  height: 220px;
+  overflow-y: auto;
 }
 </style>
