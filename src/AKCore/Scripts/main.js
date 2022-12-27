163c0994cbac706e5e9f31e1fe3c53364adeabe9
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2022-12-27 17:03:50 +0100

    Separating snow from vite build

--- src/AKCore/Scripts/main.js
+++ src/AKCore/Scripts/main.js
@@ -1,7 +1,6 @@
-import '../Styles/akstyle.scss';
-import './general.js';
-import './Vendor/object-assign-poly';
-import './Vendor/findindex-poly';
-import './vueapps.js';
-import './Admin/recruits.js';
-import './snow.js';
+import "../Styles/akstyle.scss";
+import "./general.js";
+import "./Vendor/object-assign-poly";
+import "./Vendor/findindex-poly";
+import "./vueapps.js";
+import "./Admin/recruits.js";
