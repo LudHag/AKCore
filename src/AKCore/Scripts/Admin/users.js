3e02c7da9eb1a55fb175dd07b25878bd9453cfab
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2022-12-28 12:27:14 +0100

    Removing commented code

--- src/AKCore/Scripts/Admin/users.js
+++ src/AKCore/Scripts/Admin/users.js
@@ -8,11 +8,4 @@ if (usercontainer.length > 0) {
     template: "<users-app />",
     components: { UsersApp },
   });
-  // Vue.directive('tooltip', function (el, binding) {
-  //   $(el).tooltip({
-  //     title: binding.value,
-  //     placement: binding.arg,
-  //     trigger: 'hover',
-  //   });
-  // });
 }
