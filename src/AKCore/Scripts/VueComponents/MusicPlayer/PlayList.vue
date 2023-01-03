e06cb24e5eccafb5ac9dc38b56996ff6c79bb9b5
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2023-01-03 11:53:45 +0100

    All music player things with composition API

--- src/AKCore/Scripts/VueComponents/MusicPlayer/PlayList.vue
+++ src/AKCore/Scripts/VueComponents/MusicPlayer/PlayList.vue
@@ -23,121 +23,111 @@
     </div>
   </div>
 </template>
-<script lang="ts">
-import { nextTick, defineComponent } from "vue";
+<script setup lang="ts">
+import { nextTick, defineProps, ref, computed, toRefs, watch } from "vue";
 import { nameCompare } from "../../utils/functions";
 import { Album, Track } from "./models";
 import Player from "./Player.vue";
 import PlayListItem from "./PlayListItem.vue";
 
-export default defineComponent({
-  components: {
-    Player,
-    PlayListItem,
-  },
-  props: {
-    album: {
-      type: Object as () => Album,
-      required: true,
-    },
-    playList: {
-      type: Array as () => Track[],
-      required: true,
-    },
-    playing: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      trackPlaying: undefined as Track | undefined,
-      reset: false,
-      replay: false,
-    };
-  },
-  computed: {
-    tracks() {
-      if (this.playList.length > 0) {
-        return this.playList;
-      }
-      return [...this.album.tracks]
-        .map((track) => ({ ...track, key: track.id }))
-        .sort(nameCompare);
-    },
-  },
-  watch: {
-    playList() {
-      if (this.playList.length > 0 && !this.trackPlaying) {
-        this.trackPlaying = this.playList[0];
-        nextTick(() => this.$emit("playpause"));
-      }
-    },
-  },
-  methods: {
-    next() {
-      const currentIndex = this.tracks.findIndex(
-        (track) => track.key === this.trackPlaying?.key
-      );
-      if (this.replay) {
-        return this.nextIfReplay(currentIndex);
-      }
+const emit = defineEmits<{
+  (e: "playpause"): void;
+  (e: "stop"): void;
+  (e: "remove", track: Track): void;
+  (e: "add-track", track: Track): void;
+}>();
 
-      let dontRemove = false;
-      if (currentIndex === -1 || currentIndex + 1 >= this.tracks.length) {
-        if (
-          this.playList.length > 0 &&
-          this.trackPlaying !== this.playList[0]
-        ) {
-          this.trackPlaying = this.tracks[0];
-          dontRemove = true;
-        } else {
-          this.$emit("stop");
-          this.trackPlaying = undefined;
-        }
-      } else {
-        this.trackPlaying = this.tracks[currentIndex + 1];
-      }
-      if (this.playList.length > 0 && !dontRemove) {
-        this.$emit("remove", this.playList[0]);
-      }
-    },
-    nextIfReplay(currentIndex: number) {
-      if (this.playList.length === 0) {
-        this.trackPlaying = this.tracks[currentIndex];
-        this.$emit("stop");
-        this.reset = true;
-        nextTick(() => (this.reset = false));
-        nextTick(() => this.$emit("playpause"));
-      } else {
-        if (currentIndex + 1 >= this.tracks.length) {
-          this.trackPlaying = this.tracks[0];
-        } else {
-          this.trackPlaying = this.tracks[currentIndex + 1];
-        }
-      }
-    },
-    selectTrack(track: Track) {
-      if (this.playing && this.trackPlaying === track) {
-        this.reset = true;
-        nextTick(() => (this.reset = false));
-      } else {
-        this.trackPlaying = track;
-        if (!this.playing) {
-          this.reset = true;
-          nextTick(() => (this.reset = false));
-          nextTick(() => this.$emit("playpause"));
-        }
-        if (this.playList.length > 0) {
-          const index = this.playList.indexOf(track);
-          if (!this.replay) {
-            this.$emit("remove-before", index);
-          }
-        }
-      }
-    },
-  },
+const props = defineProps<{
+  album: Album;
+  playList: Track[];
+  playing: boolean;
+}>();
+
+const { album, playList, playing } = toRefs(props);
+
+const trackPlaying = ref<Track | undefined>();
+const reset = ref(false);
+const replay = ref(false);
+
+const tracks = computed(() => {
+  if (playList.value.length > 0) {
+    return playList.value;
+  }
+  return [...album.value.tracks]
+    .map((track) => ({ ...track, key: track.id }))
+    .sort(nameCompare);
 });
+
+watch(
+  () => playList.value,
+  (val) => {
+    if (val.length > 0 && !trackPlaying.value) {
+      trackPlaying.value = val[0];
+      nextTick(() => emit("playpause"));
+    }
+  }
+);
+
+const next = () => {
+  const currentIndex = tracks.value.findIndex(
+    (track) => track.key === trackPlaying?.value?.key
+  );
+  if (replay.value) {
+    return nextIfReplay(currentIndex);
+  }
+
+  let dontRemove = false;
+  if (currentIndex === -1 || currentIndex + 1 >= tracks.value.length) {
+    if (playList.value.length > 0 && trackPlaying.value !== playList.value[0]) {
+      trackPlaying.value = tracks.value[0];
+      dontRemove = true;
+    } else {
+      emit("playpause");
+      trackPlaying.value = undefined;
+    }
+  } else {
+    trackPlaying.value = tracks.value[currentIndex + 1];
+  }
+  if (playList.value.length > 0 && !dontRemove) {
+    emit("remove", playList.value[0]);
+  }
+};
+
+const nextIfReplay = (currentIndex: number) => {
+  if (playList.value.length === 0) {
+    trackPlaying.value = tracks.value[currentIndex];
+    emit("stop");
+    reset.value = true;
+    nextTick(() => (reset.value = false));
+    nextTick(() => emit("playpause"));
+  } else {
+    if (currentIndex + 1 >= tracks.value.length) {
+      trackPlaying.value = tracks.value[0];
+    } else {
+      trackPlaying.value = tracks.value[currentIndex + 1];
+    }
+  }
+};
+
+const selectTrack = (track: Track) => {
+  if (playing.value && trackPlaying.value === track) {
+    reset.value = true;
+    nextTick(() => (reset.value = false));
+  } else {
+    trackPlaying.value = track;
+    if (!playing.value) {
+      reset.value = true;
+      nextTick(() => (reset.value = false));
+      nextTick(() => emit("playpause"));
+    }
+    if (playList.value.length > 0) {
+      const index = playList.value.indexOf(track);
+      if (!replay.value) {
+        emit("remove", playList.value[index]);
+      }
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 @import "bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss";
