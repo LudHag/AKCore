0d2b9e83da7e4f9f9b4d20f3ff93adb346400a94
Emil Jönsson <ungothar97@gmail.com>
2023-03-19 18:24:32 +0100

    Changed instrument to arsenal (#79)
    * Changed instrument to arsenal
    
    * changed order

--- src/AKCore/DataModel/AkConstants.cs
+++ src/AKCore/DataModel/AkConstants.cs
@@ -88,9 +88,9 @@ namespace AKCore.DataModel
         public const string OK = "Överkamerer";
         public const string SK = "Skrivkamerer";
         public const string KK = "Kamrerskamerer";
+        public const string Arsenalkamerer = "Arsenalkamerer";
         public const string Balettkamerer = "Balettkamerer";
         public const string Busskamerer = "Busskamerer";
-        public const string Instrumentkamerer = "Instrumentkamerer";
         public const string Kamerakamerer = "Kamerakamerer";
         public const string Kramkamerer = "Kramkamerer";
         public const string Myskamerer = "Myskamerer";
@@ -112,7 +112,7 @@ namespace AKCore.DataModel
             KK,
             Balettkamerer,
             Busskamerer,
-            Instrumentkamerer,
+            Arsenalkamerer,
             Kamerakamerer,
             Kramkamerer,
             Myskamerer,
