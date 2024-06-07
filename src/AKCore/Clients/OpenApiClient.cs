20186bea53ddd0abca1a1029b7da27ca57a0d235
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2024-06-07 20:39:44 +0200

    Allow for image lint to gpt

--- src/AKCore/Clients/OpenApiClient.cs
+++ src/AKCore/Clients/OpenApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.AI.OpenAI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +11,17 @@ public class OpenApiClient
     private readonly OpenAIClient _apiClient;
     public OpenApiClient(string token)
     {
-        _apiClient =  new OpenAIClient(token);
+        _apiClient = new OpenAIClient(token);
     }
 
-    public async Task<string> GetText(string query) { 
+    public async Task<string> GetText(string query, string imageUrl = null)
+    {
 
-        var messages = new List<ChatRequestMessage> { 
-            new ChatRequestSystemMessage("Only return the response to the question, no additional words."), 
-            new ChatRequestUserMessage(query) 
+        var messages = new List<ChatRequestMessage> {
+            new ChatRequestSystemMessage("Only return the response to the question, no additional words."),
+            imageUrl == null ?
+            new ChatRequestUserMessage(new ChatMessageTextContentItem(query)) :
+            new ChatRequestUserMessage(new ChatMessageTextContentItem(query), new ChatMessageImageContentItem(new Uri(imageUrl)))
         };
 
         var chatCompletionsOptions = new ChatCompletionsOptions("gpt-4o", messages);
