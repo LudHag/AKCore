fe89dc7c6af0b8d2e7933a13f5a4fa84734b42d6
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2024-06-25 21:41:08 +0200

    Starting structured events

---
+++ src/AKCore/Components/StructuredDataComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AKCore.DataModel;
+using AKCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKCore.Components;
+
+public class StructuredDataViewComponent : ViewComponent
+{
+    private readonly AKContext _db;
+
+    public StructuredDataViewComponent(AKContext db) => _db = db;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var gigs = await _db.Events
+                .Where(x => x.Type == "Spelning")
+                .Where(x => !x.Secret)
+                .Where(x => x.Day >= DateTime.UtcNow.Date)
+                .ToListAsync();
+
+        var orderdGigs = gigs .OrderBy(x => x.Day.Date).ThenBy(x => x.StartsTime != default ? x.StartsTime : x.HalanTime);
+                
+
+        var model = new StructuredDataModel()
+        {
+            UpcomingGigs = orderdGigs,
+        };
+        return View(model);
+    }
+}
---
+++ src/AKCore/Models/StructuredDataModel.cs
@@ -0,0 +1,13 @@
+﻿using AKCore.DataModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AKCore.Models;
+
+public record StructuredDataEvent(string Name, string Description, string StartsTime, string Place);
+
+public class StructuredDataModel
+{
+    public IEnumerable<StructuredDataEvent> UpcomingGigs { get; set; }
+}
+
