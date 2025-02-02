c9dca74449c646e319de05375a6de3acd48ae399
Emil Jönsson <ungothar97@gmail.com>
2025-02-02 19:35:44 +0100

    Feature/filter rehearsals (#162)

--- src/AKCore/Scripts/VueComponents/Upcoming/functions.ts
+++ src/AKCore/Scripts/VueComponents/Upcoming/functions.ts
@@ -1,9 +1,41 @@
-import { UpcomingEvent } from "./models";
+import { RepFilterType } from "../models";
+import { UpcomingEvent, UpcomingMonths, UpcomingYears } from "./models";
 
 export const eventIsRep = (event: UpcomingEvent) =>
-    event.type === "Rep" ||
-    event.type === "Kårhusrep" ||
-    event.type === "Athenrep" ||
-    event.type === "Fikarep" ||
-    event.type == "Balettrep" ||
-    event.type == "Samlingsrep";
+  event.type === "Rep" ||
+  event.type === "Kårhusrep" ||
+  event.type === "Athenrep" ||
+  event.type === "Fikarep" ||
+  event.type == "Balettrep" ||
+  event.type == "Samlingsrep";
+
+export const filterYears = (
+  years: UpcomingYears,
+  filter: RepFilterType,
+): UpcomingYears => {
+  const filteredYears: UpcomingYears = {};
+
+  Object.entries(years).forEach(([year, yearData]) => {
+    const filteredMonths: UpcomingMonths = {};
+
+    Object.entries(yearData.months).forEach(([month, events]) => {
+      const filteredEvents = events.filter((event) => {
+        return (
+          filter === "all" ||
+          (filter === "ballet" && event.type !== "Rep") ||
+          (filter === "orchestra" && event.type !== "Balettrep")
+        );
+      });
+
+      if (filteredEvents.length > 0) {
+        filteredMonths[month] = filteredEvents;
+      }
+    });
+
+    if (Object.keys(filteredMonths).length > 0) {
+      filteredYears[year] = { ...yearData, months: filteredMonths };
+    }
+  });
+
+  return filteredYears;
+};
--- src/AKCore/Scripts/translations/upcoming.ts
+++ src/AKCore/Scripts/translations/upcoming.ts
@@ -12,6 +12,9 @@ export const upcomingTranslations: Record<string, Translation> = {
   "ical-link": { swedish: "iCal-länk", english: "iCal-link" },
   list: { swedish: "Lista", english: "List" },
   month: { swedish: "Månad", english: "Month" },
+  allFilter: { swedish: "Alla rep", english: "All rehearsals" },
+  balletFilter: { swedish: "Balettrep", english: "Ballet rehearsals" },
+  orchestraFilter: { swedish: "Orkesterrep", english: "Orchestra rehearsals" },
   "no-concerts": {
     swedish: "Vi har tyvärr inga spelningar inplanerade närmaste tiden",
     english: "Unfortunately we have no planned concerts in the upcoming weeks",
