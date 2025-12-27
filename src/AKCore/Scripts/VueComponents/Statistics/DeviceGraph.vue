1f9717fe259c05b2196bce8ebbd99e3c38fc9f26
Ludvig Hagmar <--global>
2025-12-27 15:20:52 +0100

    Remove unused css

--- src/AKCore/Scripts/VueComponents/Statistics/DeviceGraph.vue
+++ src/AKCore/Scripts/VueComponents/Statistics/DeviceGraph.vue
@@ -43,11 +43,3 @@ const data = computed(() => ({
   ],
 }));
 </script>
-<style lang="scss" scoped>
-.spinner-container {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
