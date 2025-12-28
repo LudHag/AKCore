8167917d805fa8ca9fbca59c1240aacf2c3acc94
Ludvig Hagmar <--global>
2025-12-28 10:12:08 +0100

    No regex import

--- src/AKCore/Controllers/StatisticsController.cs
+++ src/AKCore/Controllers/StatisticsController.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AKCore.Controllers;
