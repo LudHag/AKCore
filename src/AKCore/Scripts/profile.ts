ee128cc6c9d21272130676fa4acce8bed1ffc9a5
Ludvig Hagmar <--global>
2025-12-30 23:01:50 +0100

    Extract profile page

---
+++ src/AKCore/Scripts/profile.ts
@@ -0,0 +1,8 @@
+﻿import { createApp } from "vue";
+import "vue3-select-component/styles";
+
+import ProfileApp from "./VueComponents/Profile/ProfileApp.vue";
+
+if (document.getElementById("profile-app")) {
+  createApp(ProfileApp).mount("#profile-app");
+}
