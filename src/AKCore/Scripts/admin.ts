57c9a9ae4e9a9caf8f761eaa230303232c7e2446
Ludvig Hagmar <--global>
2026-01-12 19:49:29 +0100

    Imports for components

--- src/AKCore/Scripts/admin.ts
+++ src/AKCore/Scripts/admin.ts
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
-import AdminEventApp from "./VueComponents/AdminEvent/AdminEventApp.vue";
-import AlbumEditApp from "./VueComponents/AlbumEdit/AlbumEditApp.vue";
-import MenuEditApp from "./VueComponents/MenuEdit/MenuEditApp.vue";
-import RecruitsApp from "./VueComponents/Recruits/RecruitsApp.vue";
-import UsersApp from "./VueComponents/Users/UsersApp.vue";
-import MediaApp from "./VueComponents/Media/MediaApp.vue";
-import PageEdit from "./VueComponents/PageEdit/PageEdit.vue";
-import PagesApp from "./VueComponents/Pages/PagesApp.vue";
+import AdminEventApp from "@components/AdminEvent/AdminEventApp.vue";
+import AlbumEditApp from "@components/AlbumEdit/AlbumEditApp.vue";
+import MenuEditApp from "@components/MenuEdit/MenuEditApp.vue";
+import RecruitsApp from "@components/Recruits/RecruitsApp.vue";
+import UsersApp from "@components/Users/UsersApp.vue";
+import MediaApp from "@components/Media/MediaApp.vue";
+import PageEdit from "@components/PageEdit/PageEdit.vue";
+import PagesApp from "@components/Pages/PagesApp.vue";
 import "vue3-select-component/styles";
 import "@styles/adminstyles.scss";
 
