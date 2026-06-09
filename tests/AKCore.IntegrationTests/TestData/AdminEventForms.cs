c8ed16af903c21502659942719c8e48e0bf1135a
Ludvig Hagmar <--global>
2026-06-09 10:43:35 +0200

    Make test more future proof

--- tests/AKCore.IntegrationTests/TestData/AdminEventForms.cs
+++ tests/AKCore.IntegrationTests/TestData/AdminEventForms.cs
@@ -6,13 +6,13 @@ public static class AdminEventForms
 {
     public static FormUrlEncodedContent CreateEvenemang(
         string name,
-        string day = "2030-01-01",
+        int daysFromNow = 7,
         string place = "Hålan") =>
         new(new Dictionary<string, string>
         {
             ["Type"] = AkEventTypes.Evenemang,
             ["Name"] = name,
-            ["Day"] = day,
+            ["Day"] = DateTime.UtcNow.Date.AddDays(daysFromNow).ToString("yyyy-MM-dd"),
             ["Place"] = place
         });
 }
