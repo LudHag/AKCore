00b0478d0468e885061a4fa0dfd938da6b1bdd0a
Ludvig Hagmar <--global>
2026-07-29 16:44:37 +0200

    Update

--- src/AKCore/Migrations/20170826135641_init.Designer.cs
+++ src/AKCore/Migrations/20170826135641_init.Designer.cs
@@ -13,6 +13,7 @@ namespace AKCore.Migrations
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
+#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
 
@@ -513,6 +514,7 @@ namespace AKCore.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
+#pragma warning restore 612, 618
         }
     }
 }
