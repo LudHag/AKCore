d796b03cc3670bc55d8589ef53d40bfb737eefff
Ludvig Hagmar <--global>
2026-09-04 20:11:02 +0200

    Tracking features on statistics page

---
+++ src/AKCore/Scripts/VueComponents/Statistics/UsageGraph.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="line-graph-container">
+    <Line class="line-graph" v-if="data" :data="data" :options="options" />
+    <spinner v-if="loading" class="spinner-container" :size="'large'" />
+  </div>
+</template>
+<script setup lang="ts">
+import { UsageResponse } from "./models";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  ChartOptions,
+} from "chart.js";
+
+import { Line } from "vue-chartjs";
+import { getFeatureUsageLabel, getRandomColor } from "./utils";
+import { computed } from "vue";
+import Spinner from "../Spinner.vue";
+
+const { dataPoints } = defineProps<{
+  dataPoints: UsageResponse;
+  loading: boolean;
+}>();
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+);
+
+const data = computed(() =>
+  dataPoints
+    ? {
+        labels: dataPoints.dates,
+        datasets: dataPoints.items.map((item) => {
+          return {
+            label: getFeatureUsageLabel(item.type),
+            backgroundColor: getRandomColor(item.type),
+            borderColor: getRandomColor(item.type),
+            data: item.items.map((i) => i.amount),
+          };
+        }),
+      }
+    : null,
+);
+
+const options: ChartOptions<"line"> = {
+  responsive: true,
+  maintainAspectRatio: false,
+  elements: {
+    line: {
+      cubicInterpolationMode: "monotone",
+    },
+  },
+  plugins: {
+    legend: {
+      position: "bottom",
+      labels: { color: "#fff" },
+      onClick: function (e, legendItem, legend) {
+        const index = legendItem.datasetIndex;
+        const ci = legend.chart;
+
+        if (index === undefined) {
+          return;
+        }
+
+        const someHidden = ci.data.datasets.some((dataset) => dataset.hidden);
+
+        if (someHidden) {
+          ci.data.datasets.forEach((dataset) => {
+            dataset.hidden = false;
+          });
+        } else {
+          ci.data.datasets.forEach((dataset, i) => {
+            if (i === index) {
+              dataset.hidden = false;
+            } else {
+              dataset.hidden = true;
+            }
+          });
+        }
+        ci.update();
+      },
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        color: "#fff",
+        autoSkip: true,
+        maxRotation: 0,
+        autoSkipPadding: 13,
+      },
+      grid: {
+        color: "#6d6d6d",
+      },
+    },
+    y: {
+      ticks: {
+        color: "#fff",
+        precision: 0,
+      },
+      grid: {
+        color: "#6d6d6d",
+      },
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.line-graph-container {
+  position: relative;
+  height: 300px;
+  flex-grow: 1;
+}
+.spinner-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
