a737942218539ce9e764504a719197ed72fcb120
Samuel Jonsson <ubk8751@gmail.com>
2026-09-15 17:53:04 +0200

    [ALTEKAMERE-22] Handle existing duplicate signups

--- src/AKCore/Migrations/20260911081130_AddUniqueEventMemberSignup.cs
+++ src/AKCore/Migrations/20260911081130_AddUniqueEventMemberSignup.cs
@@ -10,6 +10,23 @@ namespace AKCore.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(
+                """
+                DELETE older
+                FROM SignUps AS older
+                INNER JOIN SignUps AS newer
+                    ON newer.EventId = older.EventId
+                    AND newer.PersonId = older.PersonId
+                    AND (
+                        newer.SignupTime > older.SignupTime
+                        OR (
+                            newer.SignupTime = older.SignupTime
+                            AND newer.Id > older.Id
+                        )
+                    )
+                WHERE older.EventId IS NOT NULL;
+                """);
+
             migrationBuilder.CreateIndex(
                 name: "IX_SignUps_EventId_PersonId",
                 table: "SignUps",
